73eb950f05982016675a921bbb126bdfb6b1e941
Simon Brand <tartanllama@gmail.com>
2019-04-25 11:04:05 +0100

    Fix Doxyfile.in path for RtD

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@ import subprocess, os
 def configureDoxyfile():
 	input_dir = '../CatCutifier'
 	output_dir = 'build'
-	with open('../Doxyfile.in', 'r') as file :
+	with open('Doxyfile.in', 'r') as file :
 		filedata = file.read()
 
 	filedata = filedata.replace('@DOXYGEN_INPUT_DIR@', input_dir)
