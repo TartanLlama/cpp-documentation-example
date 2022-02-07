54d965e578ce962e3af1cdd24b3e0911db131d51
baskluiters <bas@kluiters.com>
2022-02-07 23:27:43 +0100

    make.bat to build docs
    Does not build Doxygen yet

--- activate.bat
+++ activate.bat
@@ -6,13 +6,10 @@ if not exist .my_super_project_w (
 )
 
 if not defined VIRTUAL_ENV (
-  .my_super_project_w\Scripts\activate.bat
+  call .my_super_project_w\Scripts\activate.bat
 )
 
 set DRAWIO=c:\Program Files\draw.io\draw.io.exe
 
-pip install -q --upgrade pip
-pip install -q -r ./requirements.txt
-pip install -q -r ./docs/requirements.txt
-
-cmake .
+pip install -q -r .\requirements.txt
+pip install -q -r .\docs\requirements.txt
